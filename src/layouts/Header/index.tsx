79bea9038410571d0250e8cb86aacb7da3580eaf
hppRC <hpp.ricecake@gmail.com>
2019-12-23 00:03:40 +0900

    good

--- src/layouts/Header/index.tsx
+++ src/layouts/Header/index.tsx
@@ -1,3 +1,4 @@
+import { Link } from 'gatsby';
 import React from 'react';
 
 import styled from '@emotion/styled';
@@ -5,6 +6,15 @@ import styled from '@emotion/styled';
 const Header: React.FCX = ({ className }) => (
   <header className={className}>
     <h1>Gatsby starter hpp</h1>
+    <nav>
+      <ul>
+        <li>
+          <Link to='/'>Top</Link>
+          <Link to='/about'>About</Link>
+          <Link to='/404'>404</Link>
+        </li>
+      </ul>
+    </nav>
   </header>
 );
 
@@ -12,6 +22,19 @@ export const StyledHeader = styled(Header)`
   position: fixed;
   top: 0;
   left: 0;
+
+  display: flex;
+  justify-content: space-between;
+  width: 100%;
+
+  nav {
+    padding: 2rem;
+    li {
+      a {
+        padding: 2rem;
+      }
+    }
+  }
   z-index: 1000;
 `;
 
