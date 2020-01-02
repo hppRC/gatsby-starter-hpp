be84dc934c33cfb5e10d9fa4d4c5fccb275f3ef2
hppRC <hpp.ricecake@gmail.com>
2020-01-02 16:50:39 +0900

    add definitions

--- types/gif.d.ts
+++
@@ -1,6 +0,0 @@
-declare module '*.gif' {
-  let GifComponent: string;
-  export default GifComponent;
-}
-//ex. import Gif from "example.gif"
-//    <img src={Gif}/>
---
+++ types/image.d.ts
@@ -0,0 +1,16 @@
+declare module '*.gif' {
+  const GIFsrc: string;
+  export default GIFsrc;
+}
+declare module '*.png' {
+  const PNGsrc: string;
+  export default PNGsrc;
+}
+declare module '*.jpe?g' {
+  const JPGsrc: string;
+  export default JPGsrc;
+}
+declare module '*.svg' {
+  const SVGsrc: string;
+  export default SVGsrc;
+}
