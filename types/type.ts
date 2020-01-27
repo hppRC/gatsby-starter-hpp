81a09c4dda9150258ab93d7011230da4894bd0c4
hppRC <hpp.ricecake@gmail.com>
2020-01-28 00:54:23 +0900

    chore

--- types/type.ts
+++ types/type.ts
@@ -1,6 +1,7 @@
 // please refer to this article: https://qiita.com/Takepepe/items/f66c7e2e1d22b431f148
 
-import React from 'react'
+import React from 'react';
+
 declare module 'react' {
-  type FCX<P = {}> = React.FunctionComponent<P & { className?: string }>
+  type FCX<P = {}> = React.FunctionComponent<P & { className?: string }>;
 }
